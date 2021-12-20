0248584c0a732caa24f56ae42e381bba7d1dd950
LucasOJacintho <lucasoliveirajacintho@gmail.com>
2021-12-20 19:53:13 -0300

    Criação de um campo minado em C# - Objetivo é trabalhar de forma a utilizar orientação a Objeto para resolver o problema. Campo Criado confomre seleção do jogador

---
+++ Campo minado/Mina.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campo_minado
+{
+    internal class Mina
+    {
+        public string Id { get; set; }
+        public Char Valor { get; set; }
+        public bool Visualizador { get; set; } = false;
+        public bool Analizar {get; set; } = false;
+
+        public Mina(string i, char v1, bool v2, bool v3)
+        {
+            this.Id = i;
+            this.Valor = v1;
+            this.Visualizador = v2;
+            this.Analizar = v3;
+        }
+    }
+}
---
+++ Campo minado/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Campo_minado;
+
+namespace Campo_minado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis iniciais
+            int casaAux = 0;
+            int linhaAux = 1;
+
+
+            Console.WriteLine("Digite um valor de 1 a 10: ");
+            int linha = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor de 1 a 10: ");
+            int coluna = int.Parse(Console.ReadLine());
+
+
+            List<Mina> list = new List<Mina>();
+            for (int j = 1; j <= linha; j++)
+            {
+                for (int i = 1; i <= coluna; i++)
+                {
+                    string casa = Convert.ToString(j) + Convert.ToString(i);
+                    list.AddRange(new List<Mina>
+                {
+                        new Mina(casa, '-', false, false),
+                });
+                }
+                Console.WriteLine();
+            }
+            Console.Clear();
+
+            //Imprimir linhas Superiores
+            Console.Write($"     ");
+            for (int i = 1; i <= coluna; i++)
+            {
+                if (i<10)
+                    Console.Write($" {i} ");
+                else
+                    Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Console.Write($"     ");
+            for (int i = 1; i <= coluna; i++)
+            {
+                Console.Write($" | ");
+            }
+
+            //Imprimir demais linhas
+            Console.WriteLine();
+            Console.Write($" {linhaAux} - ");
+            linhaAux++;
+            foreach (var casa in list)
+            {
+                casaAux++;
+                Console.Write($" {casa.Valor} ");
+                if (casaAux == coluna)
+                    if (casaAux == coluna)
+                    {
+                        Console.WriteLine();
+                        if (linhaAux < 10)
+                            Console.Write($" {linhaAux} - ");
+                        else
+                            if (linhaAux < coluna+1)
+                                Console.Write($"{linhaAux} - ");
+                        linhaAux++;
+                        casaAux = 0;
+                    }
+            }
+            Console.ReadLine();
+        }
+    }
+}
